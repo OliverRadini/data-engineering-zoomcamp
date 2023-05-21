233de4921bdee9d02b2082eb6d75be69b833f376
Oliver Radini <orr2@live.com>
2023-05-21 22:54:51 +0100

    Add methods to clean data pre-big-query load

--- flows/02_gcp/etl_gcs_to_bq.py
+++ flows/02_gcp/etl_gcs_to_bq.py
@@ -4,13 +4,38 @@ from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 from prefect_gcp import GcpCredentials
 
-@task()
+@task(retries=3)
 def extract_from_gcs(color, year, month):
     """Download trip data from GCS"""
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("de-zoomcamp")
     gcs_block.get_directory(from_path=gcs_path, local_path=f"../data")
-    return Path("../data/{gcs_path}")
+    return Path(f"../data/{gcs_path}")
+
+@task()
+def transform(path):
+    """data cleaning example"""
+    df = pd.read_parquet(path)
+    print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
+    df.fillna(0, inplace=True)
+    print(f"post: missing passenger count: {df['passenger_count'].isna().sum()}")
+    return df
+
+
+@task(retries=3)
+def write_bq(df):
+    """Write dataframe to big query"""
+
+    gcp_credentials_block = GcpCredentials.load("de-zoomcamp-creds")
+
+    df.to_gbq(
+        destination_table="dezoomcamp.rides",
+        project_id="sturdy-ranger-384021",
+        credentials=gcp_credentials_block.get_credentials_from_service_account(),
+        chunksize=500_000,
+        if_exists="append"
+    )
+
 
 @flow()
 def etl_gcs_to_bq():
@@ -20,6 +45,8 @@ def etl_gcs_to_bq():
     month = 1
 
     path = extract_from_gcs(color, year, month)
+    df = transform(path)
+    write_bq(df)
 
 if __name__ == "__main__":
     etl_gcs_to_bq()
