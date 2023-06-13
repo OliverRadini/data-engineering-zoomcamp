e1fc2ed380139fb72c0e15989b8157b62a15b67a
Oliver Radini <orr2@live.com>
2023-06-13 23:17:33 +0100

    Add script to move from gcs to bq

--- etl_scripts/gcs_to_bq.py
+++ etl_scripts/gcs_to_bq.py
@@ -1,27 +1,32 @@
 from google.cloud import bigquery
 
-# Construct a BigQuery client object.
-client = bigquery.Client()
+PROJECT_NAME = "sturdy-ranger-384021"
+DATASET = "dezoomcamp"
+FILE_ROOT = f"gs://dtc_data_lake_sturdy-ranger-384021/data/"
 
-# TODO(developer): Set table_id to the ID of the table to create.
-# table_id = "your-project.your_dataset.your_table_name"
 
-job_config = bigquery.LoadJobConfig(
-    schema=[
-        bigquery.SchemaField("name", "STRING"),
-        bigquery.SchemaField("post_abbr", "STRING"),
-    ],
-    skip_leading_rows=1,
-    # The source format defaults to CSV, so the line below is optional.
-    source_format=bigquery.SourceFormat.CSV,
-)
-uri = "gs://cloud-samples-data/bigquery/us-states/us-states.csv"
+# TODO: make this work with command line arguments
 
-load_job = client.load_table_from_uri(
-    uri, table_id, job_config=job_config
-)  # Make an API request.
+def gcs_to_bq(table_name, file_location):
+    client = bigquery.Client()
 
-load_job.result()  # Waits for the job to complete.
+    table_id = f"{PROJECT_NAME}.{DATASET}.{table_name}"
 
-destination_table = client.get_table(table_id)  # Make an API request.
-print("Loaded {} rows.".format(destination_table.num_rows))
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("name", "STRING"),
+            bigquery.SchemaField("post_abbr", "STRING"),
+        ],
+        skip_leading_rows=1,
+        # The source format defaults to CSV, so the line below is optional.
+        source_format=bigquery.SourceFormat.CSV,
+    )
+
+    load_job = client.load_table_from_uri(
+        f"{FILE_ROOT}{file_location}", table_id, job_config=job_config
+    )
+
+    load_job.result()
+
+    destination_table = client.get_table(table_id)
+    print("Loaded {} rows.".format(destination_table.num_rows))
--- etl_scripts/local_to_gcs.py
+++ etl_scripts/local_to_gcs.py
@@ -2,10 +2,14 @@ from google.cloud import storage
 from util.authenticate_gcp import authenticate_gcp
 from util.write_to_gcs import write_to_gcs
 
-authenticate_gcp("sturdy-ranger-384021")
+BUCKET_NAME = "dtc_data_lake_sturdy-ranger-384021"
+PROJECT_NAME = "sturdy-ranger-384021"
+
+# TODO: make this work with command line arguments
 
 def local_to_gcs(file_location, contents):
-    PROJECT_NAME = "dtc_data_lake_sturdy-ranger-384021"
-    write_to_gcs(PROJECT_NAME, f"data/{file_location}", contents)
+    write_to_gcs(BUCKET_NAME, f"data/{file_location}", contents)
+
+authenticate_gcp(PROJECT_NAME)
 
 local_to_gcs("test/one.md", "## hello, world")
